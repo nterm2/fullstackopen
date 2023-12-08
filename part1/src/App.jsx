91905afd63a60db5c07cc75518ef0e1f561f71fa
Neil Martey <neil.marteybotchway@gmail.com>
2023-12-08 07:28:44 +0000

    complete exercise 1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,21 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.firstExercises + props.secondExercises + props.thirdExercises}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +27,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content part={part1} exercise={exercises1}/>
+      <Content part={part2} exercise={exercises2}/>
+      <Content part={part3} exercise={exercises3}/>
+      <Total firstExercises={exercises1} secondExercises={exercises2} thirdExercises={exercises3}/>
     </div>
   )
 }
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,4 +1,5 @@
-import ReactDom from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+
 import App from './App'
 
-ReactDom.createRoot(document.getElementById('root').render(<App />))
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
