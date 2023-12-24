45c02b9afd0289c992bfbf5b6ef4caa53b22e4ef
Neil Martey <neil.marteybotchway@gmail.com>
2023-12-24 15:34:48 +0000

    Submission for exercise 3.5

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+app.use(express.json())
 let persons = [
     { 
       "id": 1,
@@ -31,6 +31,7 @@ app.get('/api/persons/', (request, response) => {
 app.get('/info', (request, response) => {
     const requestReceptionTime = new Date()
     const numOfPeople = persons.length 
+    console.log(persons)
     response.send(`
     <p>Phonebook has info for ${numOfPeople} people</p>
     <p>${requestReceptionTime}</p>
@@ -52,6 +53,19 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
+})
+
+function generateId() {
+    return Math.random() * 1000 
+}
+app.post('/api/persons', (request, response) => {
+    const person = request.body 
+    console.log(person)
+    console.log(person)
+    const id = generateId()
+    person.id = id
+    persons = persons.concat(person)
+    response.status(200).end()
 })
 const PORT=3004
 app.listen(PORT, () => {
