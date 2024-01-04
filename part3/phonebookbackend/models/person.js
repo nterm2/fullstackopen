9de26d5dafcafbafc9d9945ee588939cfe57021a
Neil Martey <neil.marteybotchway@gmail.com>
2024-01-04 14:36:50 +0000

    Submission for exercises 3.15

--- part3/phonebookbackend/models/person.js
+++ part3/phonebookbackend/models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const password = process.env.DB_PASSWORD
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.lungpw3.mongodb.net/phonebookApps?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:cooldude123@cluster0.lungpw3.mongodb.net/phonebookApps?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
@@ -13,4 +13,12 @@ const phonebookSchema = new mongoose.Schema(
     }
 ) 
 
+phonebookSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+  
 module.exports = mongoose.model('Person', phonebookSchema)
